a0b9ac9cc12a471c66b3bec738c1bd54c8d9f9ab
poetovio <urban.vizintin@outlook.com>
2022-10-29 00:20:40 +0200

    Delujoc datum

--- app/src/main/java/com/example/projekt/MainActivity.kt
+++ app/src/main/java/com/example/projekt/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.projekt
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.Spinner
+import android.widget.TextView
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
+
+    private val koledar = Calendar.getInstance()
+    private val formatter = SimpleDateFormat("dd-MM-yyyy", Locale.GERMAN)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.gumb_date).setOnClickListener {
+            DatePickerDialog(this, this, koledar.get(Calendar.YEAR), koledar.get(Calendar.MONTH), koledar.get(Calendar.DAY_OF_MONTH)).show()
+        }
 
         val spolSpinner = findViewById<Spinner>(R.id.spinner2)
 
@@ -29,5 +43,14 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         }
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        koledar.set(year, month, dayOfMonth)
+        prikaziDatum(koledar.timeInMillis)
+    }
+
+    private fun prikaziDatum(timestamp: Long) {
+        findViewById<TextView>(R.id.textView6).text = formatter.format(timestamp)
     }
 }
