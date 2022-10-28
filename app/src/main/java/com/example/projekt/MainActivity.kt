f6764aa9b38d2e2d4e81bf6834fcd4a4cfd65e50
poetovio <urban.vizintin@outlook.com>
2022-10-28 23:19:31 +0200

    Vzpostavitev repozitorija -> dela spinner ter gumb

---
+++ app/src/main/java/com/example/projekt/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.projekt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spolSpinner = findViewById<Spinner>(R.id.spinner2)
+
+
+
+        val gumbPoslji = findViewById<Button>(R.id.buttonPoslji)
+
+        spolSpinner.adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.spol,
+            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item
+        )
+        gumbPoslji.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    Povzetek::class.java
+                )
+            )
+        }
+    }
+}
