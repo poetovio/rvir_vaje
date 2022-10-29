870e4b95cf3437a9fd10c6941199733600e9b37b
poetovio <urban.vizintin@outlook.com>
2022-10-29 16:57:16 +0200

    Gumb nazaj -> deluje

--- app/src/main/java/com/example/projekt/Povzetek.kt
+++ app/src/main/java/com/example/projekt/Povzetek.kt
@@ -1,11 +1,24 @@
 package com.example.projekt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Povzetek : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_povzetek)
+
+        val gumbNazaj = findViewById<Button>(R.id.gumb_nazaj)
+
+        gumbNazaj.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    MainActivity::class.java
+                )
+            )
+        }
     }
 }
